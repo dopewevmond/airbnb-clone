25cd04e841ff5415d30cb306e4b4c7bd6fc9ae42
dopewevmond <dopewevmond@gmail.com>
2022-12-05 12:06:35 +0000

    unused exceptions

--- backend/src/exceptions/exception.noaccesstoken.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from './exception.http'
-
-class NoAccessTokenException extends HttpException {
-  constructor () {
-    super(401, 'Bearer access token not found in request header')
-  }
-}
-
-export default NoAccessTokenException
--- backend/src/exceptions/exception.unauthorized.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from './exception.http'
-
-class UnauthorizedException extends HttpException {
-  constructor () {
-    super(403, 'You are not authorized to perform this action')
-  }
-}
-
-export default UnauthorizedException
