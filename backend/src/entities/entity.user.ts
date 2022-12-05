dee9eb8f85c5090dd7b6997e56fd4794fb5d2037
dopewevmond <dopewevmond@gmail.com>
2022-12-05 16:55:03 +0000

    allow users to set nullable fields back to null

--- backend/src/entities/entity.user.ts
+++ backend/src/entities/entity.user.ts
@@ -14,14 +14,14 @@ class User {
   @Column('varchar', { length: 50 })
     last_name: string
 
-  @Column('varchar', { length: 255 })
+  @Column('varchar', { length: 255, unique: true })
     email_address: string
 
   @Column('varchar', { length: 100 })
     password_hash: string
 
   @Column('varchar', { length: 12, nullable: true })
-    phone_number: string
+    phone_number: string | null
 
   @Column({ type: 'timestamptz' })
     created_at: Date
@@ -30,13 +30,13 @@ class User {
     native_language: LanguageType
 
   @Column({ type: 'enum', enum: languages, nullable: true })
-    secondary_language: LanguageType
+    secondary_language: LanguageType | null
 
   @Column('varchar', { length: 255, nullable: true })
-    bio: string
+    bio: string | null
 
   @Column('varchar', { length: 255, nullable: true })
-    profile_photo: string
+    profile_photo: string | null
 
   @Column({ type: 'boolean', default: false })
     has_verified_email: boolean
