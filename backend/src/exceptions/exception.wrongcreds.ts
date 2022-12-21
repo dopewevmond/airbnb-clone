9bc019a7f70ae64b492e5f94e4dd0d2e587ecd06
dopewevmond <dopewevmond@gmail.com>
2022-12-21 12:28:54 +0000

    better login error message

--- backend/src/exceptions/exception.wrongcreds.ts
+++ backend/src/exceptions/exception.wrongcreds.ts
@@ -2,7 +2,7 @@ import HttpException from './exception.http'
 
 class WrongCredentialsException extends HttpException {
   constructor () {
-    super(401, 'Wrong credentials provided')
+    super(401, 'Wrong username or password')
   }
 }
 
